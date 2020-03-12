4027b7acfdc7efa9a78d2a9d1242995aa530b9b1
dkStephanos <dkstephanos@gmail.com>
2020-03-12 15:57:31 -0400

    Adding first lab

---
+++ lab_1_problem/plotData.m
@@ -0,0 +1,35 @@
+function plotData(x, y)
+%PLOTDATA Plots the data points x and y into a new figure 
+%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
+%   population and profit.
+
+figure; % open a new figure window
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the training data into a figure using the 
+%               "figure" and "plot" commands. Set the axes labels using
+%               the "xlabel" and "ylabel" commands. Assume the 
+%               population and revenue data have been passed in
+%               as the x and y arguments of this function.
+%
+% Hint: You can use the 'rx' option with plot to have the markers
+%       appear as red crosses. Furthermore, you can make the
+%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+
+fprintf('Plotting Data ...\n')
+data = load('ex1data1.txt');
+X = data(:, 1); y = data(:, 2);
+m = length(y); % number of training examples
+
+% Plot Data
+% Note: You have to complete the code in plotData.m
+plot(X, y, "xr");
+xlabel("Population of City in 10,000s");
+ylabel("Profit in $10,000s");
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+% ============================================================
+
+end
